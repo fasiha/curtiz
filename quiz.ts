0b398598f2b8eb475557ffbd6a8cb9ac557e8520
Ahmed Fasih <wuzzyview@gmail.com>
2018-09-13 21:59:38 -0400

    Generalize cloze-deletion answers a bit

--- quiz.ts
+++ quiz.ts
@@ -1,5 +1,5 @@
 import {fill} from './cliFillInTheBlanks';
-import {MaybeMorpheme, ultraCompressMorpheme} from './mecabUnidic';
+import {Morpheme, ultraCompressMorpheme} from './mecabUnidic';
 import {
   BunsetsuBlock,
   Content,
@@ -33,44 +33,49 @@ function fillHoles<T>(a: T[], b: T[], predicate: (a: T) => boolean = (o => !o)) 
   return a;
 }
 
+async function cloze(clozes: Array<string|null>): Promise<string[]> {
+  let numberOfParticles = 0;
+  let printableCloze = clozes.map(o => o ? o : `(${++numberOfParticles})`);
+  console.log(`Fill in the numbered blanks:\n${printableCloze.join('')}\nEnter text or "# text":`);
+  let responses: string[] = Array.from(Array(numberOfParticles), _ => '');
+  await fill((thisResponse: string) => {
+    // Most expected use case: type "a" enter "b" enter to fill in two blanks.
+    // Less likely: "2 b" enter "a" enter to fill in the second blank first.
+    // ("1 a" above should be fine too)
+    let numHit = thisResponse.match(/^([0-9]+)/);
+    if (numHit) {
+      let num = parseInt(numHit[0]);
+      if (num <= numberOfParticles) {
+        // Don't expand the array
+        let rest = thisResponse.slice(numHit[0].length).trim();
+        responses[num - 1] = rest;
+      }
+    } else {
+      let firstEmpty = responses.findIndex(o => !o);
+      if (firstEmpty >= 0) {
+        responses[firstEmpty] = thisResponse.trim();
+      } else {
+        responses.push(thisResponse.trim());
+      }
+    }
+    console.log(fillHoles(clozes.slice(), responses).map((c, i) => c ? c : printableCloze[i]).join(''));
+    return responses.every(o => !!o); // exit criteria
+  });
+  return responses;
+}
+
 async function administerQuiz(toQuiz: Quizzable, mode: string) {
   if (toQuiz instanceof SentenceBlock) {
     if (mode === 'particle') {
       let particles = new Set(toQuiz.particleMorphemes.map(ultraCompressMorpheme));
       let clozes: (string|null)[] =
           toQuiz.morphemes.map(m => particles.has(ultraCompressMorpheme(m)) ? null : (m ? m.literal : ''));
-      let numberOfParticles = 0;
-      let printableCloze = clozes.map(o => o ? o : `(${++numberOfParticles})`);
-
-      let responses: string[] = Array.from(Array(numberOfParticles), _ => '');
-      console.log(`Fill in the numbered blanks:\n${printableCloze.join('')}\nEnter text or "# text":`);
-      await fill((thisResponse: string) => {
-        // Most expected use case: type "a" enter "b" enter to fill in two blanks.
-        // Less likely: "2 b" enter "a" enter to fill in the second blank first.
-        // ("1 a" above should be fine too)
-        let numHit = thisResponse.match(/^([0-9]+)/);
-        if (numHit) {
-          let num = parseInt(numHit[0]);
-          if (num <= numberOfParticles) {
-            // Don't expand the array
-            let rest = thisResponse.slice(numHit[0].length).trim();
-            responses[num - 1] = rest;
-          }
-        } else {
-          let firstEmpty = responses.findIndex(o => !o);
-          if (firstEmpty >= 0) {
-            responses[firstEmpty] = thisResponse.trim();
-          } else {
-            responses.push(thisResponse.trim());
-          }
-        }
-        console.log(fillHoles(clozes.slice(), responses).map((c, i) => c ? c : printableCloze[i]).join(''));
-        return responses.every(o => !!o); // exit criteria
-      });
+      let responses: string[] = await cloze(clozes);
       fillHoles(clozes, responses);
-      console.log(clozes);
+      console.log('quiz responses', responses);
     } else if (mode === 'conjugation') {
-      const bunsetsuToString = (b: MaybeMorpheme[]) => b.map(m => m && m.literal).join('');
+      const bunsetsuToString = (b: Morpheme[]) => b.map(m => m.literal).join('');
+      console.log(toQuiz)
       let bunsetsus = new Set(toQuiz.conjugatedBunsetsus.map(bunsetsuToString));
       let number = 1;
       let toPrint =
